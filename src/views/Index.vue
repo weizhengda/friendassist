8c1535deff4fef95b9e7b2605339d4c58f9bf5f5
weizhengda <2625785357@qq.com>
2018-09-12 22:03:43 +0800

    0912

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,20 +2,26 @@
     <div id="index">
          <div class="rule">活动规则</div>
          <div class="join">参加活动</div>
+         <!-- 登陆 -->
+         <login v-if="isLogin"></login>
     </div>
 </template>
 <script>
+    import Login from '@/views/Login'
     export default{
         name:'Index',
+        components:{
+            login:Login
+        },
         data(){
             return{
-               title:'好友助力'
+               isLogin:false
             }
         }
     }
 </script>
 <style lang="less">
-  @import '../../static/css/index.less';
+  @import '../../static/css/index';
 </style>
 
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,14 @@
 <template>
-    <div id="login">
-
+    <div id="login_wrap">
+        <div class="login">
+          <div class="title">立即参加好友助力活动</div>
+          <div class="phone">
+              <input v-model="tel" type="number">
+          </div>
+          <div class="code">
+              <input v-model="code" type="text">
+          </div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,8 +16,13 @@
         name:'Login',
         data(){
             return{
-
+               tel:'',  // 手机号码
+               code:''  // 验证码
             }
         }
     }
 </script>
+<style lang="less">
+   @import '../../static/css/login';
+</style>
+
